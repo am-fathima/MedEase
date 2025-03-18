daa11606b9245cfb50f4c571f8e51c9be0f2b964
RosaRahmath <s22009928@ousl.lk>
2025-03-18 16:26:14 +0530

    button

--- bookingform.php
+++ bookingform.php
@@ -78,7 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MedEase - Appointment Form</title>
-    <link rel="stylesheet" href="bookingform.css"> <!-- Retaining your CSS -->
+    <link rel="stylesheet" href="bookingform.css"> 
 </head>
 <body>
     <div class="container">
